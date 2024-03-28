3bdd776f7df2c88f48e74f27037f1ee837c9e2ea
PK006 <plai6588@gmail.com>
2024-03-28 15:57:03 +0700

    Update Statistic.py

--- pages/Statistic.py
+++ pages/Statistic.py
@@ -5,7 +5,7 @@ import requests
 import pandas as pd
 
 dt=pd.read_csv('./data/apple.csv')
-st.write(dt.head(10))
+
 
 def load_lottieurl(url: str):
     r = requests.get(url)
@@ -18,3 +18,13 @@ lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello,key="hello")
 
 st.subheader("การนำเสนอข้อมูลด้วยสถิติ")
+st.write(dt.head(50))
+st.write('ผลรวม')
+cl1,cl2,cl3,cl4.cl5,cl6,cl7=st.columns(7)
+cl1.write(dt['Size'].sum())
+cl2.write(dt['Weight'].sum())
+cl3.write(dt['Sweetness'].sum())
+cl4.write(dt['Crunchiness'].sum())
+cl5.write(dt['Juiciness'].sum())
+cl6.write(dt['Ripeness'].sum())
+cl7.write(dt['Acidity'].sum())
