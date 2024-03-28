751f6de926d681cfbb15ac37bcbb25a553dcb4e1
PK006 <plai6588@gmail.com>
2024-03-28 15:45:50 +0700

    ใ

--- pages/Chart.py
+++ pages/Chart.py
@@ -14,3 +14,11 @@ lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello,key="hello")
 
 st.subheader("การนำเสนอข้อมูลด้วยการจินตทัศน์ข้อมูล")
+
+html_1 = """
+<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>กราฟแสดงข้อมูลแอปเปิ้ล</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
