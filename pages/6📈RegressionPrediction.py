814be072a23d5665d0aa948ab9da9c261b0362c2
PK006 <plai6588@gmail.com>
2024-03-29 14:20:52 +0700

    .

--- pages/6📈RegressionPrediction.py
+++ pages/6📈RegressionPrediction.py
@@ -16,6 +16,7 @@ st.write(df.head(10))
 
 #st.line_chart(df)
 #st.line_chart(df, x="interest_rate", y="unemployment_rate", color="stock_index_price")
+st.image('./pic/stock2.jpg')
 st.header("กราฟแสดงข้อมูล stock index price")
 st.line_chart(
    df, x="interest_rate", y=["stock_index_price"], color=["#FF0000"]  # Optional
